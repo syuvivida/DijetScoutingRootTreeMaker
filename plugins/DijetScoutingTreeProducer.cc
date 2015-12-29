76a45b5e2d16cf4285a5667c54602dc7a496fdd8
David Sheffield <sheffield@physics.rutgers.edu>
2015-12-29 13:32:08 -0600

    Add EDAnalyzer to create scouting ntuple

---
+++ plugins/DijetScoutingTreeProducer.cc
@@ -0,0 +1,266 @@
+#include "CMSDIJET/DijetScoutingRootTreeMaker/plugins/DijetScoutingTreeProducer.h"
+
+using namespace std;
+using namespace edm;
+
+
+DijetScoutingTreeProducer::DijetScoutingTreeProducer(const ParameterSet& cfg):
+    doJECs_(cfg.getParameter<bool>("doJECs")),
+    ptMinAK4_(cfg.getParameter<double>("ptMinAK4")),
+    srcJetsAK4_(consumes<ScoutingPFJetCollection>(
+                    cfg.getParameter<InputTag>("jetsAK4")))
+{
+
+}
+
+DijetScoutingTreeProducer::~DijetScoutingTreeProducer()
+{
+}
+
+
+void DijetScoutingTreeProducer::beginJob()
+{
+    //--- book the tree -----------------------
+    outTree_ = fs_->make<TTree>("events","events");
+    outTree_->Branch("runNo"                ,&run_               ,"run_/I");
+    outTree_->Branch("evtNo"                ,&evt_               ,"evt_/I");
+    outTree_->Branch("lumi"                 ,&lumi_              ,"lumi_/I");
+    outTree_->Branch("nvtx"                 ,&nVtx_              ,"nVtx_/I");
+    outTree_->Branch("rho"                  ,&rho_               ,"rho_/F");
+    outTree_->Branch("met"                  ,&met_               ,"met_/F");
+    outTree_->Branch("metSig"               ,&metSig_            ,"metSig_/F");
+
+    outTree_->Branch("nJetsAK4"           ,&nJetsAK4_          ,"nJetsAK4_/I");
+    outTree_->Branch("htAK4"              ,&htAK4_             ,"htAK4_/F");
+    outTree_->Branch("mjjAK4"             ,&mjjAK4_            ,"mjjAK4_/F");
+    outTree_->Branch("dEtajjAK4"          ,&dEtajjAK4_         ,"dEtajjAK4_/F");
+    outTree_->Branch("dPhijjAK4"          ,&dPhijjAK4_         ,"dPhijjAK4_/F");
+
+    ptAK4_             = new std::vector<float>;
+    jecAK4_            = new std::vector<float>;
+    etaAK4_            = new std::vector<float>;
+    phiAK4_            = new std::vector<float>;
+    massAK4_           = new std::vector<float>;
+    energyAK4_         = new std::vector<float>;
+    areaAK4_           = new std::vector<float>;
+    chfAK4_            = new std::vector<float>;
+    nhfAK4_            = new std::vector<float>;
+    phfAK4_            = new std::vector<float>;
+    mufAK4_            = new std::vector<float>;
+    elfAK4_            = new std::vector<float>;
+    nemfAK4_           = new std::vector<float>;
+    cemfAK4_           = new std::vector<float>;
+    // Hadronic forward hadrons
+    hf_hfAK4_          = new std::vector<float>;
+    // Hadronic forward electromagnetic fraction
+    hf_emfAK4_         = new std::vector<float>;
+    hofAK4_            = new std::vector<float>;
+    idLAK4_            = new std::vector<int>;
+    idTAK4_            = new std::vector<int>;
+    chHadMultAK4_     = new std::vector<int>;
+    chMultAK4_         = new std::vector<int>;
+    neHadMultAK4_      = new std::vector<int>;
+    neMultAK4_         = new std::vector<int>;
+    phoMultAK4_        = new std::vector<int>;
+
+    outTree_->Branch("jetPtAK4"                ,"vector<float>"     ,&ptAK4_);
+    outTree_->Branch("jetJecAK4"               ,"vector<float>"     ,&jecAK4_);
+    outTree_->Branch("jetEtaAK4"               ,"vector<float>"     ,&etaAK4_);
+    outTree_->Branch("jetPhiAK4"               ,"vector<float>"     ,&phiAK4_);
+    outTree_->Branch("jetMassAK4"              ,"vector<float>"     ,&massAK4_);
+    outTree_->Branch("jetEnergyAK4"            ,"vector<float>"     ,&energyAK4_);
+    outTree_->Branch("jetAreaAK4"              ,"vector<float>"     ,&areaAK4_);
+    outTree_->Branch("jetChfAK4"               ,"vector<float>"     ,&chfAK4_);
+    outTree_->Branch("jetNhfAK4"               ,"vector<float>"     ,&nhfAK4_);
+    outTree_->Branch("jetPhfAK4"               ,"vector<float>"     ,&phfAK4_);
+    outTree_->Branch("jetMufAK4"               ,"vector<float>"     ,&mufAK4_);
+    outTree_->Branch("jetElfAK4"               ,"vector<float>"     ,&elfAK4_);
+    outTree_->Branch("jetNemfAK4"              ,"vector<float>"     ,&nemfAK4_);
+    outTree_->Branch("jetCemfAK4"              ,"vector<float>"     ,&cemfAK4_);
+    outTree_->Branch("jetHf_hfAK4"             ,"vector<float>"     ,&hf_hfAK4_);
+    outTree_->Branch("jetHf_emfAK4"            ,"vector<float>"    ,&hf_emfAK4_);
+    outTree_->Branch("jetHofAK4"               ,"vector<float>"    ,&hofAK4_);
+    outTree_->Branch("idLAK4"                  ,"vector<int>"      ,&idLAK4_);
+    outTree_->Branch("idTAK4"                  ,"vector<int>"      ,&idTAK4_);
+    outTree_->Branch("chHadMultAK4"          ,"vector<int>"      ,&chHadMultAK4_);
+    outTree_->Branch("chMultAK4"              ,"vector<int>"      ,&chMultAK4_); 
+    outTree_->Branch("neHadMultAK4"           ,"vector<int>"      ,&neHadMultAK4_);
+    outTree_->Branch("neMultAK4"              ,"vector<int>"      ,&neMultAK4_);
+    outTree_->Branch("phoMultAK4"             ,"vector<int>"      ,&phoMultAK4_);
+}
+
+
+void DijetScoutingTreeProducer::endJob()
+{
+}
+
+
+void DijetScoutingTreeProducer::analyze(const Event& iEvent,
+                                        const EventSetup& iSetup)
+{
+    initialize();
+
+    // Get collections
+
+    Handle<ScoutingPFJetCollection> jetsAK4;
+    iEvent.getByToken(srcJetsAK4_, jetsAK4);
+    if (!jetsAK4.isValid()) {
+        throw edm::Exception(edm::errors::ProductNotFound)
+            << "Could not find ScoutingPFJetCollection." << endl;
+        return;
+    }
+
+    //-------------- Event Info -----------------------------------
+    run_    = iEvent.id().run();
+    evt_    = iEvent.id().event();
+    lumi_   = iEvent.id().luminosityBlock();
+
+    //-------------- Jets -----------------------------------------
+    vector<double> jecFactorsAK4;
+    vector<unsigned> sortedAK4JetIdx;
+    if(doJECs_) {
+
+    } else {
+        for(ScoutingPFJetCollection::const_iterator ijet=jetsAK4->begin();
+            ijet!=jetsAK4->end(); ++ijet) {
+            jecFactorsAK4.push_back(1.0);
+            sortedAK4JetIdx.push_back(ijet - jetsAK4->begin());
+        }
+    }
+
+    nJetsAK4_ = 0;
+    float htAK4 = 0.0;
+    vector<TLorentzVector> vP4AK4;
+    for (vector<unsigned>::const_iterator i=sortedAK4JetIdx.begin();
+         i!=sortedAK4JetIdx.end(); ++i) {
+        ScoutingPFJetCollection::const_iterator ijet = (jetsAK4->begin() + *i);
+        TLorentzVector jet;
+        jet.SetPtEtaPhiM(ijet->pt(), ijet->eta(), ijet->phi(), ijet->m());
+
+        double jet_energy = jet.E();
+        double chf = ijet->chargedHadronEnergy()/jet_energy;
+        double nhf = ijet->neutralHadronEnergy()/jet_energy;
+        double phf = ijet->photonEnergy()/jet_energy;
+        double elf = ijet->electronEnergy()/jet_energy;
+        double muf = ijet->muonEnergy()/jet_energy;
+
+        double hf_hf = ijet->HFHadronEnergy()/jet_energy;
+        double hf_emf= ijet->HFEMEnergy()/jet_energy;
+        double hof   = ijet->HOEnergy()/jet_energy;
+
+        int chm    = ijet->chargedHadronMultiplicity();
+
+        int chMult = ijet->chargedHadronMultiplicity()
+                   + ijet->electronMultiplicity() + ijet->muonMultiplicity();
+        int neMult = ijet->neutralHadronMultiplicity()
+                   + ijet->photonMultiplicity();
+        int npr    = chMult + neMult;
+
+        int chHadMult = chm;
+        int neHadMult = ijet->neutralHadronMultiplicity();
+        int phoMult = ijet->photonMultiplicity();
+
+        // Juska's added fractions for identical JetID with recommendations
+        double nemf = ijet->photonEnergy()/jet_energy;
+        double cemf = (ijet->electronEnergy() + ijet->muonEnergy())/jet_energy;
+        int NumConst = npr;
+
+        float eta  = ijet->eta();
+        float pt   = ijet->pt()*jecFactorsAK4.at(*i);
+
+        // https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetID
+        int idL = (nhf < 0.99 && nemf < 0.99 && NumConst > 1)
+            && ((fabs(eta) <= 2.4 && chf > 0 && chMult > 0 && cemf < 0.99)
+                || fabs(eta) > 2.4);
+        int idT = (nhf < 0.90 && nemf < 0.90 && NumConst > 1)
+            && ((fabs(eta) <= 2.4 && chf > 0 && chMult > 0 && cemf < 0.90)
+                || fabs(eta) > 2.4);
+
+        if (pt > ptMinAK4_) {
+            htAK4 += pt;
+            ++nJetsAK4_;
+
+            vP4AK4.push_back(jet);
+            chfAK4_           ->push_back(chf);
+            nhfAK4_           ->push_back(nhf);
+            phfAK4_           ->push_back(phf);
+            elfAK4_           ->push_back(elf);
+            mufAK4_           ->push_back(muf);
+            nemfAK4_          ->push_back(nemf);
+            cemfAK4_          ->push_back(cemf);
+            hf_hfAK4_         ->push_back(hf_hf);
+            hf_emfAK4_        ->push_back(hf_emf);
+            hofAK4_           ->push_back(hof);
+            jecAK4_           ->push_back(jecFactorsAK4.at(*i));
+            ptAK4_            ->push_back(pt);
+            phiAK4_           ->push_back(ijet->phi());
+            etaAK4_           ->push_back(ijet->eta());
+            massAK4_          ->push_back(ijet->m()*jecFactorsAK4.at(*i));
+            energyAK4_        ->push_back(jet_energy*jecFactorsAK4.at(*i));
+            areaAK4_          ->push_back(ijet->jetArea());
+            idLAK4_           ->push_back(idL);
+            idTAK4_           ->push_back(idT);
+            chHadMultAK4_     ->push_back(chHadMult);
+            chMultAK4_        ->push_back(chMult);
+            neHadMultAK4_     ->push_back(neHadMult);
+            neMultAK4_        ->push_back(neMult);
+            phoMultAK4_       ->push_back(phoMult);
+        }
+    }
+    htAK4_ = htAK4;
+    if (nJetsAK4_ > 1) { // Assuming jets are ordered by pt
+        mjjAK4_    = (vP4AK4[0] + vP4AK4[1]).M();
+        dEtajjAK4_ = fabs(etaAK4_->at(0) - etaAK4_->at(1));
+        dPhijjAK4_ = fabs(deltaPhi(phiAK4_->at(0), phiAK4_->at(1)));
+    }
+
+    //---- Fill Tree ---
+    outTree_->Fill();
+    //------------------
+}
+
+
+void DijetScoutingTreeProducer::initialize()
+{
+    run_            = -999;
+    evt_            = -999;
+    lumi_           = -999;
+    nVtx_           = -999;
+    rho_            = -999;
+    met_            = -999;
+    metSig_         = -999;
+    nJetsAK4_          = -999;
+    htAK4_             = -999;
+    mjjAK4_            = -999; 
+    dEtajjAK4_         = -999; 
+    dPhijjAK4_         = -999;
+    ptAK4_             ->clear();
+    etaAK4_            ->clear();
+    phiAK4_            ->clear();
+    massAK4_           ->clear();
+    energyAK4_         ->clear();
+    areaAK4_           ->clear();
+    chfAK4_            ->clear();
+    nhfAK4_            ->clear();
+    phfAK4_            ->clear();
+    elfAK4_            ->clear();
+    mufAK4_            ->clear();
+    nemfAK4_           ->clear();
+    cemfAK4_           ->clear();
+    hf_hfAK4_             ->clear();
+    hf_emfAK4_            ->clear();
+    hofAK4_            ->clear();
+    jecAK4_            ->clear();
+    jecAK4_            ->clear();
+    idLAK4_            ->clear();
+    idTAK4_            ->clear();
+    // Juska's fix
+    chHadMultAK4_     ->clear();
+    chMultAK4_        ->clear();
+    neHadMultAK4_     ->clear();
+    neMultAK4_        ->clear();
+    phoMultAK4_        ->clear();
+}
+
+
+DEFINE_FWK_MODULE(DijetScoutingTreeProducer);
---
+++ prod/flat-data_cfg.py
@@ -0,0 +1,74 @@
+import FWCore.ParameterSet.Config as cms 
+
+process = cms.Process('jetToolbox')
+
+process.load('PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+
+## ----------------- Global Tag ------------------
+#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+#process.GlobalTag.globaltag = THISGLOBALTAG
+
+
+#--------------------- Report and output ---------------------------
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+
+THISROOTFILE = "tree.root"
+process.TFileService=cms.Service("TFileService",
+                                 fileName=cms.string(THISROOTFILE),
+                                 closeFileFast = cms.untracked.bool(True)
+                                 )
+
+process.options = cms.untracked.PSet(
+        allowUnscheduled = cms.untracked.bool(True),
+        wantSummary = cms.untracked.bool(False),
+)
+
+############## output  edm format ###############
+process.out = cms.OutputModule('PoolOutputModule',
+                               fileName = cms.untracked.string('jettoolbox.root'),
+                               outputCommands = cms.untracked.vstring([
+                                                                      'keep *_slimmedJets_*_*',
+                                                                      'keep *_slimmedJetsAK8_*_*',
+                                                                      ])
+                               )
+
+#### NOT RUNNING OUTPUT MODULE ######
+# process.endpath = cms.EndPath(process.out)
+
+
+##-------------------- Define the source  ----------------------------
+
+
+
+process.source = cms.Source(
+    "PoolSource",
+    fileNames = cms.untracked.vstring('root://cmsxrootd-site.fnal.gov//store/user/dgsheffi/ScoutingPFHT/ScoutingPFHT_E037F887-2678-E511-8C8F-02163E0136BE.root')
+)
+
+
+
+##-------------------- User analyzer  --------------------------------
+
+
+process.dijetscouting = cms.EDAnalyzer(
+    'DijetScoutingTreeProducer',
+    ## JETS/MET ########################################
+    jetsAK4 = cms.InputTag('hltScoutingPFPacker'),
+    ptMinAK4         = cms.double(10),
+
+    ## JECs ################
+    doJECs  = cms.bool(False)
+)
+
+
+# ------------------ path --------------------------
+
+process.p = cms.Path(process.dijetscouting)
