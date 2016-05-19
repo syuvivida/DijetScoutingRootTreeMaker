8adbac9ba7122f02de61fdf524023cf6104642ad
federico de guio <federico.de.guio@cern.ch>
2016-05-19 11:51:22 +0200

    make easier the configuration of the HLT paths to monitor

--- prod/flat-data-calo_cfg.py
+++ prod/flat-data-calo_cfg.py
@@ -9,8 +9,8 @@ process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cf
 
 ## ----------------- Global Tag ------------------
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
-#process.GlobalTag.globaltag = "80X_dataRun2_HLT_v12"
-process.GlobalTag.globaltag = THISGLOBALTAG
+process.GlobalTag.globaltag = "80X_dataRun2_HLT_v12"
+#process.GlobalTag.globaltag = THISGLOBALTAG
 
 
 #--------------------- Report and output ---------------------------
@@ -21,8 +21,8 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.TFileService=cms.Service("TFileService",
-                                 fileName=cms.string(THISROOTFILE),
-                                 #fileName=cms.string("dijetNtuple.root"),
+                                 #fileName=cms.string(THISROOTFILE),
+                                 fileName=cms.string("dijetNtuple.root"),
                                  closeFileFast = cms.untracked.bool(True)
                                  )
 
@@ -41,11 +41,13 @@ process.source = cms.Source(
     )
 )
 
-##--- l1 stage2 digis ---
+##------------------- l1 stage2 digis ------------------------------
 process.load("EventFilter.L1TRawToDigi.gtStage2Digis_cfi")
 process.gtStage2Digis.InputLabel = cms.InputTag( "hltFEDSelectorL1" )
 
 ##-------------------- User analyzer  --------------------------------
+#import trigger conf
+from CMSDIJET.DijetScoutingRootTreeMaker.HLTpaths_cfi import getConf
 
 process.dijetscouting = cms.EDAnalyzer(
     'DijetCaloScoutingTreeProducer',
@@ -59,101 +61,10 @@ process.dijetscouting = cms.EDAnalyzer(
     doRECO     = cms.bool(False),
 
     ## trigger ###################################
-    triggerAlias = cms.vstring(
-        # Scouting
-        'CaloJet40_CaloScouting_PFScouting', 
-        'L1HTT_CaloScouting_PFScouting', 
-        'CaloScoutingHT250', 
-        # RECO 
-        'PFHT800', 
-        'PFHT650', 
-        'PFHT600',
-        'PFHT475',
-        'PFHT400',
-        'PFHT350',
-        'PFHT300',
-        'PFHT250',
-        'PFHT200',
-        'PFHT650MJJ950',
-        'PFHT650MJJ900',
-        'PFJET500',
-        'PFJET450',
-        'PFJET200',
-        'HT2000',
-        'HT2500',
-        'Mu45Eta2p1',
-        'AK8PFHT700TriMass50',
-        'AK8PFJet360TrimMass50',
-        'CaloJet500NoJetID',
-        'DiPFJetAve300HFJEC',
-        'DiPFJetAve500',
-        'PFHT750FourJetPt50',
-        'QuadPFJetVBF'
-    ),
-    triggerSelection = cms.vstring(
-        # Scouting
-        'DST_CaloJet40_CaloScouting_PFScouting_v*',
-        'DST_L1HTT_CaloScouting_PFScouting_v*',
-        'DST_HT250_CaloScouting_v*',
-        # RECO
-        'HLT_PFHT800_v*',
-        'HLT_PFHT650_v*',
-        'HLT_PFHT600_v*',
-        'HLT_PFHT475_v*',
-        'HLT_PFHT400_v*',
-        'HLT_PFHT350_v*',
-        'HLT_PFHT300_v*',
-        'HLT_PFHT250_v*',
-        'HLT_PFHT200_v*',
-        'HLT_PFHT650_WideJetMJJ950DEtaJJ1p5_v*',
-        'HLT_PFHT650_WideJetMJJ900DEtaJJ1p5_v*',
-        'HLT_PFJet500_v*',
-        'HLT_PFJet450_v*',
-        'HLT_PFJet200_v*',
-        'HLT_HT2000_v*',
-        'HLT_HT2500_v*',
-        'HLT_Mu45_eta2p1_v*',
-        'HLT_AK8PFHT700_TrimR0p1PT0p03Mass50_v*',
-        'HLT_AK8PFJet360_TrimMass30_v*',
-        'HLT_CaloJet500_NoJetID_v*',
-        'HLT_DiPFJetAve300_HFJEC_v*',
-        'HLT_DiPFJetAve500_v*',
-        'HLT_PFHT750_4JetPt50_v*',
-        'HLT_QuadPFJet_VBF_v*'
-    ),
-    triggerDuplicates = cms.vint32(
-        ## If >0, trigger result will be ORed with result from the previous
-        ## trigger rather than being push_backed
-        # Scouting
-        0, # CaloJet40_CaloScouting_PFScouting
-        0, # L1HTT_CaloScouting_PFScouting
-        0, # CaloScoutingHT250
-        # RECO
-        0, # PFHT800
-        0, # PFHT650
-        0, # PFHT600
-        0, # PFHT475
-        0, # PFHT400
-        0, # PFHT350
-        0, # PFHT300
-        0, # PFHT250
-        0, # PFHT200
-        0, # PFHT650MJJ950
-        0, # PFHT650MJJ900
-        0, # PFJET500
-        0, # PFJET450
-        0, # PFJET200
-        0, # HT2000
-        0, # HT2500
-        0, # Mu45Eta2p1
-        0, # AK8PFHT700TriMass50
-        0, # AK8PFJet360TrimMass50
-        0, # CaloJet500NoJetID
-        0, # DiPFJetAve300HFJEC
-        0, # DiPFJetAve500
-        0, # PFHT750FourJetPt50
-        0  # QuadPFJetVBF'
-    ),
+    triggerAlias = cms.vstring(getConf(0)),
+    triggerSelection = cms.vstring(getConf(1)),
+    triggerDuplicates = cms.vint32(getConf(2)),
+
     triggerConfiguration = cms.PSet(
         hltResults            = cms.InputTag('TriggerResults','','HLT'),
         l1tResults            = cms.InputTag(''),
